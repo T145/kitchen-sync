3788d99f7203e00a2afd47225957d3eaa02bb367
T145 <T145@protonmail.com>
2024-07-14 04:16:41 -0400

    fix(script): Fixed script runtime

--- scripts/kitchen-sync.bash
+++ scripts/kitchen-sync.bash
@@ -8,6 +8,7 @@ shopt -s extglob      # enable extended pattern matching (https://www.gnu.org/so
 set -euET -o pipefail # put bash into strict mode & have it give descriptive errors
 umask 055             # change all generated file perms from 755 to 700
 export LC_ALL=C       # force byte-wise sorting and default langauge output
+export GIT_TERMINAL_PROMPT=0  # https://stackoverflow.com/questions/65163081/how-do-i-stop-git-from-asking-credentials-when-i-try-to-clone-a-repository-that
 
 CACHE=$(mktemp -d)
 ADLISTS='dist/adlists.txt'
@@ -23,40 +24,74 @@ github_query() {
 
 round_up() {
     echo "num = ${1};base = num / 1;if (((num - base) * 10) > 0) base += 1;print base;" | bc
-    #echo ''
 }
 
 manage_lists() {
+    local dirname;
+    local exit_code;
+    local fname;
+
     while IFS= read -r repo_url; do
+        set +e # Temporarily disable strict fail, in case web requests fail
         git clone "$repo_url"
 
-        mlr --csv cut -f address 'my-pihole-lists/adlist.csv' >>"$ADLISTS"
-        mlr --csv cut -f domain 'my-pihole-lists/domainlist.csv' >>"$DOMAINS"
+        exit_code=$?
 
-        rm -rf my-pihole-lists/
+        if [ $exit_code -eq 0 ]; then
+            dirname="$(basename ${repo_url%.*})"
+
+            set -e # Enable error checks to fail if something bad happens while processing lists
+
+            fname="${dirname}/adlist.csv"
+
+            if [ -f "$fname" ]; then
+                mlr --csv --headerless-csv-output cut -f address "$fname" >>"$ADLISTS"
+            fi
+
+            fname="${dirname}/domainlist.csv"
+
+            if [ -f "$fname" ]; then
+                mlr --csv --headerless-csv-output cut -f address "$fname" >>"$DOMAINS"
+            fi
+
+            fname="${dirname}/adlists.txt"
+
+            if [ -f "$fname" ]; then
+                mawk '/^[^[:space:]|^#|^!|^;|^$|^:|^*]/{print $1}' "$fname" >>"$ADLISTS"
+            fi
+
+            fname="${dirname}/blacklist.txt"
+
+            if [ -f "$fname" ]; then
+                mawk '/^[^[:space:]|^#|^!|^;|^$|^:|^*]/{print $1}' "$fname" >>"$DOMAINS"
+            fi
+
+            rm -rf "$dirname"
+        else
+            set -e
+        fi
     done
 }
 
 sorted() {
-    parsort -bfiu -S 100% --parallel=200000 -T "$CACHE" "$1" | sponge "$1"
+    mawk '{$1=$1};1' "$1" | parsort -bfiu -S 100% -T "$CACHE" | sponge "$1"
 }
 
 main() {
     local network_count
     local page_count
 
-    network_count="$(github_query https://api.github.com/repos/stevejenkins/my-pihole-lists | jq -r '.forks')"
+    network_count="$(github_query https://api.github.com/repos/stevejenkins/my-pihole-lists | jaq -r '.forks')"
     page_count="$(echo "${network_count}/30" | bc -l)"
     page_count="$(round_up "$page_count")"
 
     # 30 is the default page count when querying forks
     # trying to increase it up to 100 doesn't work well
-    rm -r dist/
-    mkdir dist/
+    mkdir -p dist/
 
     seq "$page_count" | while IFS= read -r page; do
         github_query "https://api.github.com/repos/stevejenkins/my-pihole-lists/forks?page=${page}" |
-            jq -r '.[].clone_url' | manage_lists
+            jaq -r '.[].clone_url' | manage_lists
     done
 
     sorted "$ADLISTS"
